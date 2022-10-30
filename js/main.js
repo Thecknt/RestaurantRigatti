7d0dcc789070de0589cb5481c74c77e26eed2529
Cristian <barretocristianr@gmail.com>
2022-10-30 20:55:28 -0300

    minimal change

--- js/main.js
+++ js/main.js
@@ -11,13 +11,25 @@ window.addEventListener("load",() => {
         navigator.geolocation.getCurrentPosition((position)=>{
             console.log(position);
             lon = position.coords.longitude;
-            lat = position.lat.latitude;
+            lat = position.coords.latitude;
 
             //ID Api
 
-            const api= ce47e98113bf9cbdec55d51255db67d7;
+            const api= "ce47e98113bf9cbdec55d51255db67d7";
 
-            const url_base = 'https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ce47e98113bf9cbdec55d51255db67d7';
+            const url_base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&` + 
+            `lon=${lon}&appid=${api}`;
+
+            fetch(url_base)
+            .then((response) => {
+
+                return response.json();
+            })
+
+            .then((data) =>{
+                console.log("Esta es la Data")
+                console.log(data);
+            })
         })
     }
 })
